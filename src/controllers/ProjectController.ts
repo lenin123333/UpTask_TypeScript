9e02ff7d773e1aa17d9da246a94c47dbcacd02af
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-02 17:40:32 -0600

    modified:   src/controllers/ProjectController.ts

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -3,7 +3,7 @@ import type { Request,Response } from "express"
 export class ProjectController{
 
     static createProject= async(req:Request,res:Response)=>{
-        res.send('Creadno proeycto')
+       
     }
 
     static getAllProjects = async(req:Request,res:Response)=>{
