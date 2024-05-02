84ac96108da8784a369a4cb03236950e23ecbbde
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-02 17:32:40 -0600

    modified:   .gitignore 	new file:   src/controllers/ProjectController.ts 	new file:   src/models/Project.ts 	new file:   src/routes/projectRoutes.ts 	modified:   src/server.ts

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { ProjectController } from "../controllers/ProjectController";
+const router = Router();
+
+router.get('/', ProjectController.getAllProjects )
+
+export default router
