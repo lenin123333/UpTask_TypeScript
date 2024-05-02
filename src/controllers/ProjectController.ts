c0fd1249ab3995cb84e74d94e052cddb94d081c5
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-02 17:36:23 -0600

    modified:   src/controllers/ProjectController.ts 	modified:   src/routes/projectRoutes.ts

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,11 @@
 import type { Request,Response } from "express"
 
 export class ProjectController{
+
+    static createProject= async(req:Request,res:Response)=>{
+        res.send('Creadno proeycto')
+    }
+
     static getAllProjects = async(req:Request,res:Response)=>{
         res.send('todos los proyectos')
     }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
 const router = Router();
 
+router.post('/', ProjectController.createProject )
 router.get('/', ProjectController.getAllProjects )
 
 export default router
