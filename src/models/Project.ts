a65d6803d973698abd9fdfbdbf5500e6885af34a
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-08 20:59:26 -0600

    modified:   src/models/Project.ts new file:   src/models/Task.ts

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,28 +1,36 @@
-import mongoose,{ Schema,Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
 
-export type ProjectType = Document & {
-    projectName:string
-    clientName:string
-    description:string
+export interface IProject extends Document {
+    projectName: string
+    clientName: string
+    description: string,
+    tasks: PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
-    projectName:{
-        type:String,
-        require:true,
-        trim:true
+    projectName: {
+        type: String,
+        require: true,
+        trim: true
     },
-    clientName:{
-        type:String,
-        require:true,
-        trim:true
+    clientName: {
+        type: String,
+        require: true,
+        trim: true
     },
-    description:{
-        type:String,
-        require:true,
-        trim:true
+    description: {
+        type: String,
+        require: true,
+        trim: true
     },
-})
+    task: [
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+}, { timestamps: true })
 
-const Project = mongoose.model<ProjectType>('Project', ProjectSchema)
+const Project = mongoose.model<IProject>('Project', ProjectSchema)
 export default Project
---
+++ src/models/Task.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+//Dicionario
+const taskStatus={
+    PENDING:'pending',
+    ON_HOLD:'onHold',
+    IN_PROGRESS:'inProgress',
+    UNDER_REVIEW:'underReview',
+    COMPLETED:'completed'
+} as const
+//Solo recibe un dato que sea como los de arriba
+export type TaskStauts=typeof taskStatus[keyof typeof taskStatus]
+
+export interface ITask extends Document {
+    name: string
+    description: string
+    project: Types.ObjectId,
+    staus:TaskStauts
+}
+
+export const TaskSchema: Schema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        require: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+        require: true,
+    },
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
+    },
+    staus:{
+        type:String,
+        enum:Object.values(taskStatus),
+        default:taskStatus.PENDING
+    }
+}, { timestamps: true })
+
+const Task = mongoose.model<ITask>('Task', TaskSchema);
+export default Task;
