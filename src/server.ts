9524fc8ace0587a8b6a5ac630867b3ea45d4ba25
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-16 18:25:10 -0600

    Configuracion de cors en el servidor

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import  express  from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db";
 import projectRoutes from './routes/projectRoutes'
-
+import cors from 'cors'
+import { corsConfig } from "./config/cors";
 dotenv.config()
 connectDB()
 const app = express()
+app.use(cors(corsConfig))
 app.use(express.json())
 //routes
 app.use('/api/projects',projectRoutes);
